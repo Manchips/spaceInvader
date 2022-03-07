e839846ca255b9e728d8249944a579de3339a626
Manchips <78320081+Manchips@users.noreply.github.com>
2022-03-06 23:10:33 -0800

    Player/Invaders
    Added player movement and shooting and made the invaders rows and cols they move but just to the right I have the godown function too but I need to set it up with the movement function

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    //this is gonna be empty for right now but we'll add the animation later, actually wont be empty for the on die degaltes and stuff but I made this early on lol 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,29 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class 
+    PlayerController : MonoBehaviour
 {
-    private float speed = 2f;
-    private Rigidbody body;
+    private float speed = 7f;
+    //private Rigidbody body holdover from when I was using the axis 
     public GameObject bullet;
     public Transform shottingOffset;
-    
-    void Start()
-    {
-        body = GetComponent<Rigidbody>();
-    }
     
     // Update is called once per frame
     void Update()
     {
-        //gonna get the horizontal keys since its 2d 
-        float axis = Input.GetAxis("Horizontal");
-        body.AddForce(Vector3.right * axis * speed, ForceMode.Force);
-        
-        if (Mathf.Abs(axis) < 0.1f)
+        //gonna get the horizontal keys since its 2d actually stratch that hated movement with the axis so 
+        //were just gonna use the getkey for if the button is held down or not 
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            float newX = body.velocity.x * (1f - Time.deltaTime * 5f);
-            body.velocity = new Vector3(newX, body.velocity.y, body.velocity.z);
+            transform.position += Vector3.left * this.speed * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * this.speed * Time.deltaTime;
         }
         
         //checks for space to shot the bullet
